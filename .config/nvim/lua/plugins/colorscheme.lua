d22c902039a1a6f41bfde9e0a42f688a9f162831
mercenarioZ <funnystar320@gmail.com>
2025-07-09 07:30:28 +0700

    feat(theme): change theme to catppuccin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,24 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  opts = function()
-    return {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    }
-  end,
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = true,
+		opts = function()
+			return {
+				transparent = true,
+				styles = {
+					sidebars = "transparent",
+					floats = "transparent",
+				},
+			}
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "mocha", -- or "latte", "frappe", "macchiato"
+			transparent_background = true,
+		},
+	},
 }
