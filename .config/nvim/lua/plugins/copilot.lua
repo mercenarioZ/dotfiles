aebcd57accaaf70927f3c65b280a97b3cb41b93d
mercenarioZ <funnystar320@gmail.com>
2025-07-13 22:50:54 +0700

    feat(copilot): copilot config, remove vim-cmp config

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,15 +1,24 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        enabled = true,
-
-      },
-      panel = { enabled = true },
-    })
-  end,
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	build = ":Copilot auth",
+	event = "BufReadPost",
+	opts = {
+		suggestion = {
+			enabled = true,
+			auto_trigger = true,
+			hide_during_completion = vim.g.ai_cmp,
+			keymap = {
+				accept = false,
+				next = "<M-]>",
+				prev = "<M-[>",
+			},
+		},
+		panel = { enabled = false },
+		filetypes = {
+			markdown = true,
+			help = true,
+		},
+	},
 }
+
