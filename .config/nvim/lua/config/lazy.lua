793b098fa08421ec6e8635a201e4c92f30799e04
mercenarioZ <funnystar320@gmail.com>
2026-03-19 00:35:40 +0700

    feat: add kotlin

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -29,8 +29,8 @@ require("lazy").setup({
 		},
 
 		-- { import = "lazyvim.plugins.extras.formatting.prettier" },
-		-- { import = "lazyvim.plugins.extras.lang.typescript" },
-		-- { import = "lazyvim.plugins.extras.lang.tailwind" },
+		-- { import = "lazyvim.plugins.extras..typescript" },
+		{ import = "lazyvim.plugins.extras.lang.kotlin" },
 
 		-- import/override with your plugins
 		{ import = "plugins" },
