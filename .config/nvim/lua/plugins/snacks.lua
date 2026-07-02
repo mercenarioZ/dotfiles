a9e83c34c03f4a180e304392fb5e63a8b10c5aa4
mercenarioZ <funnystar320@gmail.com>
2026-07-02 10:53:21 +0700

    feat(nvim): show dotfiles on explorer and grep

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,5 +2,12 @@ return {
 	"folke/snacks.nvim",
 	opts = {
 		explorer = { hidden = true },
+		picker = {
+			sources = {
+				explorer = { hidden = true },
+				files = { hidden = true },
+				grep = { hidden = true },
+			},
+		},
 	},
 }
