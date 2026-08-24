b5eb6a135394fec115d96fc3320be96967b68df2
mercenarioZ <funnystar320@gmail.com>
2026-08-24 17:43:10 +0700

    chore(nvim): reduce explorer width

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,7 +4,18 @@ return {
 		explorer = { hidden = true, ignored = true },
 		picker = {
 			sources = {
-				explorer = { hidden = true, ignored = true },
+				explorer = {
+					hidden = true,
+					ignored = false,
+					layout = {
+						preset = "sidebar",
+						preview = false,
+						layout = {
+							width = 32,
+							min_width = 20,
+						},
+					},
+				},
 				files = { hidden = true, ignored = true },
 				grep = { hidden = true, ignored = true },
 			},
