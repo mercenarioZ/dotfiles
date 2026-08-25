a94190a461a8655b4b5c83c817f329cc622501c3
mercenarioZ <funnystar320@gmail.com>
2026-08-26 00:09:00 +0700

    chore(nvim): hide jj metadata on snacks picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,7 +1,11 @@
 return {
 	"folke/snacks.nvim",
 	opts = {
-		explorer = { hidden = true, ignored = true },
+		explorer = {
+			hidden = true,
+			ignored = false,
+			exclude = { ".git", ".git/**", "*/.git/*", ".jj", ".jj/**", "*/.jj/*" },
+		},
 		picker = {
 			sources = {
 				explorer = {
@@ -18,6 +22,21 @@ return {
 				},
 				files = { hidden = true, ignored = true },
 				grep = { hidden = true, ignored = true },
+				explorer = {
+					hidden = true,
+					ignored = false,
+					exclude = { ".git", ".git/**", "*/.git/*", ".jj", ".jj/**", "*/.jj/*" },
+				},
+				files = {
+					hidden = true,
+					ignored = false,
+					exclude = { ".git", ".git/**", "*/.git/*", ".jj", ".jj/**", "*/.jj/*" },
+				},
+				grep = {
+					hidden = true,
+					ignored = false,
+					exclude = { ".git", ".git/**", "*/.git/*", ".jj", ".jj/**", "*/.jj/*" },
+				},
 			},
 		},
 		scroll = { enabled = false },
