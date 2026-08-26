948e30cee9458bf4abba7cf7157b7443a9f25b29
mercenarioZ <funnystar320@gmail.com>
2026-08-26 13:23:59 +0700

    feat(nvim): add telescope file browser

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,6 +9,15 @@ return {
 					return vim.fn.executable("make") == 1
 				end,
 			},
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+
+		keys = {
+			{
+				"<leader>sf",
+				"<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>",
+				desc = "File Browser",
+			},
 		},
 
 		config = function()
@@ -34,9 +43,16 @@ return {
 						no_ignore_parent = true,
 					},
 				},
+				extensions = {
+					file_browser = {
+						hidden = { file_browser = true, folder_browser = true },
+						file_ignore_patterns = { "/%.git/", "/%.jj/", "%.jj$" },
+					},
+				},
 			})
 
 			pcall(telescope.load_extension, "fzf")
+			pcall(telescope.load_extension, "file_browser")
 		end,
 	},
 }
