47b2755204d435cbe124e23efe74227e250d6ed6
mercenarioZ <funnystar320@gmail.com>
2026-09-16 23:18:39 +0700

    refactor(nvim): make vim.pack config the default
    Moves the vim.pack config from .config/nvim-pack to .config/nvim, and
    keeps the LazyVim config as .config/nvim-lazyvim. The old one still runs
    with NVIM_APPNAME=nvim-lazyvim.

--- .config/nvim-pack/init.lua
+++
@@ -1,7 +0,0 @@
--- bytecode cache: speeds up every require()
-vim.loader.enable()
-
-require("nai.config.options")
-require("nai.config.keymaps")
-require("nai.config.autocmds")
-require("nai.plugins")
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,7 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- bytecode cache: speeds up every require()
+vim.loader.enable()
 
-require("config.tabline").setup()
-vim.o.showtabline = 2
-vim.o.tabline = "%!v:lua.require('config.tabline').render()"
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-	group = vim.api.nvim_create_augroup("CustomTabline", { clear = true }),
-	callback = function()
-		require("config.tabline").setup()
-	end,
-})
+require("nai.config.options")
+require("nai.config.keymaps")
+require("nai.config.autocmds")
+require("nai.plugins")
