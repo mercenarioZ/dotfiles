c2fb7a364e267d7a6f5c27f43b9d5d78811a800a
mercenarioZ <funnystar320@gmail.com>
2026-09-16 18:19:38 +0700

    feat(nvim-pack): add blink.cmp completion
    Ports LazyVim's blink defaults: enter preset, lsp/path/snippets/buffer
    sources, auto docs and cmdline completion for : only.
    
    Pinned to ^1 release tags so the prebuilt rust fuzzy matcher downloads
    instead of needing cargo.

--- .config/nvim-pack/lua/nai/plugins/init.lua
+++ .config/nvim-pack/lua/nai/plugins/init.lua
@@ -7,9 +7,13 @@ vim.pack.add({
 	{ src = gh("folke/snacks.nvim") },
 	{ src = gh("mason-org/mason.nvim") },
 	{ src = gh("neovim/nvim-lspconfig") },
+	{ src = gh("rafamadriz/friendly-snippets") },
+	-- release tags ship a prebuilt fuzzy matcher, main needs cargo
+	{ src = gh("saghen/blink.cmp"), version = vim.version.range("^1") },
 })
 
 vim.cmd.colorscheme("tokyonight")
 
 require("nai.plugins.snacks")
 require("nai.plugins.lsp")
+require("nai.plugins.blink")
